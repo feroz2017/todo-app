effee25f697193b08888b0acda75f7c89dace87d
feroz <mmferoz.13mu@gmail.com>
2023-10-22 13:10:03 +0500

    add: delete operation

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 // Sample data store (an array to simulate a simple database)
-const dataStore = [];
+const dataStore2 = [];
 
 // Create (Insert) Operation
 function createData(item) {
-  dataStore.push(item);
   console.log(`Item added: ${item}`);
+  dataStore2.push(item);
 }
+
+const deleteOpe = () => {
+  console.log("delete operation");
+};
 
 // Read (Retrieve) Operation
 function readData() {
-  console.log("Data in the data store:");
-  dataStore.forEach((item, index) => {
+  console.log("Data in the data store:121212");
+  dataStore2.forEach((item, index) => {
     console.log(`Item ${index + 1}: ${item}`);
   });
 }
